2d15b9d067aaefc251fa83baa36cc7237643b75e
David Hervas <bioestadistica@iislafe.es>
2018-07-07 18:40:16 +0200

    Clarification of comments

--- pca_y_tsne.R
+++ pca_y_tsne.R
@@ -26,7 +26,7 @@ ggplot(pca_loadings, aes(x=id, y=loading, color=pc))+geom_line()+facet_wrap(~pc)
 #Proyecciones para cada observación
 pca1@scores
 
-#Visualización con plot base
+#Visualización de la proyección de las observaciones con plot base
 plot(pca1@scores, pch=16)
 #batch effect?
 plot(pca1@scores, pch=16, col=c("red", "blue")[unclass(datos_mirna$batch)], las=1)
