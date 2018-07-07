46b46592bde096ee31590e7281b3eca2b9a324bc
David Hervas <bioestadistica@iislafe.es>
2018-07-07 15:53:57 +0200

    Added NMF package

--- instalacion_paquetes.R
+++ instalacion_paquetes.R
@@ -1,6 +1,6 @@
 #Instalación de paquetes necesarios
 
 install.packages(c("glmnet", "randomForest", "clickR", "pbapply", "mixOmics",
-                   "sNPLS", "Rtsne", "car", "ordinal"))
+                   "sNPLS", "Rtsne", "car", "ordinal", "NMF"))
 source("https://bioconductor.org/biocLite.R")
 biocLite("pcaMethods")
