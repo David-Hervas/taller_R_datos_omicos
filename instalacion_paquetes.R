a0a5f7da4e412d6f147705051e0993a68e3dadf6
David Hervas <bioestadistica@iislafe.es>
2018-07-08 15:36:52 +0200

    Added ggplot2 to install.packages()

--- instalacion_paquetes.R
+++ instalacion_paquetes.R
@@ -2,6 +2,6 @@
 
 install.packages(c("glmnet", "randomForest", "clickR", "pbapply", "mixOmics",
                    "sNPLS", "Rtsne", "car", "ordinal", "NMF", "ranger",
-                   "BootValidation"))
+                   "BootValidation", "ggplot2"))
 source("https://bioconductor.org/biocLite.R")
 biocLite("pcaMethods")
